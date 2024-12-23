ce30d2c314977dc37b08afe34c729928f900197c
s0lica <vasiliudavid321@gmail.com>
2024-12-23 22:53:50 +0200

    testfinalsper

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -71,6 +71,4 @@ func Login_user(w http.ResponseWriter, r *http.Request) {
 		var tmpl = template.Must(template.ParseFiles("templates/probleme.html"))
 		tmpl.Execute(w, nil)
 	}
-	var geani = 10
-	fmt.Println(geani)
 }
