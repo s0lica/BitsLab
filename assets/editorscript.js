e0c1945a9fc0bf425f4387698108458f6d389ad9
s0lica <vasiliudavid321@gmail.com>
2025-01-04 21:36:43 +0200

    CodeMirror styling complete, solved problemtemplate styling issues.

--- assets/editorscript.js
+++ assets/editorscript.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var editor = CodeMirror.fromTextArea(document.getElementById("task_description"), {
       lineNumbers: true,
-      mode: "markdown" ,
+      mode: {
+        name:"markdown" ,
+        highlighFormatting: true,
+        extraModes:{
+          name:"stex",
+          delimiters: [["$$", "$$"]],
+        }
+      },
+      value: ""
   });
 });
